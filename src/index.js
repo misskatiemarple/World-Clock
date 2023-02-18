d875540690844b91164f4b1ad24e155cfdd70b8b
misskatiemarple <misskatiemarple@gmail.com>
2023-02-18 14:37:55 +0900

    Tokyo html added

--- src/index.js
+++ src/index.js
@@ -15,6 +15,14 @@ function updateTime() {
 
   parisDateElement.innerHTML = parisTime.format("dddd, Do MMMM YYYY");
   parisTimeElement.innerHTML = parisTime.format("h:mm:ss[<small>] A[</small>]");
+
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+
+  tokyoDateElement.innerHTML = tokyoTime.format("dddd, Do MMMM YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss[<small>] A[</small>]");
 }
 updateTime();
 setInterval(updateTime, 1000);
